dda5733123c42400035be0d696a9d66e68b8b69f
chandra <chandra@gmail.com>
2022-07-23 21:42:45 +0530

    second commit

---
+++ MVCFirstproject/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFirstproject.Models;
+
+namespace MVCFirstproject.Controllers
+{
+    public class EmployeeController : Controller
+    
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+
+            Employee employee = new Employee()
+            {
+                EmployeeId = 1001,
+                EmployeeName = "chandra",
+                 EmployeeLocation = "banglore"
+            };
+            return View(employee);
+            
+        }
+    }
+}
---
+++ MVCFirstproject/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFirstproject.Models
+{
+    public class Employee 
+    {
+        public int EmployeeId 
+        {
+            get;
+            set; 
+        }
+        public string EmployeeName 
+        { 
+            get;
+            set;
+        }
+        public string EmployeeLocation 
+        { 
+            get;
+            set;
+        }
+    }
+}
